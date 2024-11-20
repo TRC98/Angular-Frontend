ba6a47d3975db078464b1a06614fd8c10bbccb3f
Rajitha Chamod <trchamod98@gmail.com>
2024-11-20 16:24:22 +0530

    few changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',// this is the custom html tag for the component. When you need to use component it should access with this name
+  standalone: true, 
+  imports: [RouterOutlet,RolesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Frontend';
+  name = 'Chamod';
 }
